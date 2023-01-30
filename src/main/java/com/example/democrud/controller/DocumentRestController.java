c1081500ba681c146ba77f328fe927d7b2bc48c3
kenneth <kennethgut777@gmail.com>
2023-01-29 20:01:19 -0600

    Remove HTML and resources css

--- src/main/java/com/example/democrud/controller/DocumentRestController.java
+++ src/main/java/com/example/democrud/controller/DocumentRestController.java
@@ -1,6 +1,9 @@
 package com.example.democrud.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,43 +16,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.democrud.model.Persona;
-import com.example.democrud.service.api.PersonaServiceAPI;
+import com.example.democrud.model.Document;
+import com.example.democrud.service.api.FileUploadService;
 
 @RestController
-@RequestMapping(value = "/api/v1/")
+@RequestMapping(value = "/api/v1/file/upload/")
 @CrossOrigin("*")
 public class DocumentRestController {
 
 	@Autowired
-	private PersonaServiceAPI personaServiceAPI;
+	 private FileUploadService fileUploadService;
 
-	@GetMapping(value = "/all")
-	public List<Persona> getAll() {
-		return personaServiceAPI.getAll();
-	}
-	
-	@GetMapping(value = "/find/{id}")
-	public Persona find(@PathVariable Long id) {
-		return personaServiceAPI.get(id);
-	}
-
-	@PostMapping(value = "/save")
-	public ResponseEntity<Persona> save(@RequestBody Persona persona) {
-		Persona obj = personaServiceAPI.save(persona);
-		return new ResponseEntity<Persona>(obj, HttpStatus.OK);
-	}
-
-	@GetMapping(value = "/delete/{id}")
-	public ResponseEntity<Persona> delete(@PathVariable Long id) {
-		Persona persona = personaServiceAPI.get(id);
-		if (persona != null) {
-			personaServiceAPI.delete(id);
-		} else {
-			return new ResponseEntity<Persona>(HttpStatus.NO_CONTENT);
+	@GetMapping(value = "/upload")
+	public ResponseEntity<Document> handleFileUpload(@Valid @RequestBody Document doc) {
+		Document object = new Document();
+		if(doc.getFile().getSize() > 0) {
+			try {
+				fileUploadService.uploadFile(doc.getFile().getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		return new ResponseEntity<Persona>(persona, HttpStatus.OK);
+		else {
+			return new ResponseEntity<Document>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<Document>(object, HttpStatus.OK);
 	}
-
 }
